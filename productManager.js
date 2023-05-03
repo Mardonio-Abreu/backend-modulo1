4fcb3146171149744cf972c3c7f2f66213514e93
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-05-03 16:55:47 -0600

    getProducts implemetation

--- productManager.js
+++ productManager.js
@@ -26,7 +26,32 @@ class ProductManager{
 
             }
 
+        getProducts = () => {
+            for (let i = 0; i < this.products.length; i++){
+                console.log(this.products[i].title);
+                console.log(this.products[i].description);
+                console.log(this.products[i].price);
+                console.log(this.products[i].thumbnail);
+                console.log(this.products[i].code);
+                console.log(this.products[i].stock);
+            }
         }
+
+        getProductsById = (id_number) => {
+
+            console.log(this.products[i].title);
+                console.log(this.products[id_number].description);
+                console.log(this.products[id_number].price);
+                console.log(this.products[id_number].thumbnail);
+                console.log(this.products[id_number].code);
+                console.log(this.products[id_number].stock);
+        }
+
+        
+
+        }
+
+
 
 
            
