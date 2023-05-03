d6460b681f1e3571d41932af3648cd45afe42df5
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-05-03 13:17:57 -0600

    "Product repeated code filter implemented"

--- productManager.js
+++ productManager.js
@@ -9,16 +9,34 @@ class ProductManager{
         
         let productCode = code;
 
+        for (let i = 0; i < this.products.length; i++){
+            if (productCode === this.products[i].code){
+                console.log("Código de producto utilizado anteriormente!");
+                         }}
+
+
+        try{
+               this.products.push({title, description, price, thumbnail, code, stock});
+            }
+        catch(e){
+                console.log("Error");
+                
+            }
+
+
+            }
+
+        }
+
+
+           
         
-        
-        this.products.push({title, description, price, thumbnail, code, stock});
 
 
 
-    }
-
+    
 
 
 
 
-}
+
