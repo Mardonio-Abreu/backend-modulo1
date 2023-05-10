a7b3ce28483c3dd972333be97fb4cfa81dd2b4a8
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-05-10 13:10:58 -0600

    Final implementation, tests passed!

--- productManager.js
+++ productManager.js
@@ -4,10 +4,12 @@ class ProductManager{
     constructor(){
 
         this.products = [];
+        this.id = 0;
                 
     }
 
-           addProduct = (title, description, price, thumbnail, code, stock) => {
+           
+        addProduct (title, description, price, thumbnail, code, stock) {
 
         try { if(title.length == 0 || description.length == 0 || price.length == 0 || thumbnail.length == 0 || code.length == 0 || stock.length == 0){console.log("Surprise MotherFather!");}
             }
@@ -18,35 +20,69 @@ class ProductManager{
                        
         }
 
-        
-              
-        this.products.push({title, description, price, thumbnail, code, stock});
+        let flag = true;
+
+        this.products.forEach((product)=>{
+            if (product.code === code){
+                flag = false;
+                console.log("Code already in use!");
+            }
+        })
+
+        if (flag){
+
+        let id = this.id++;
+        this.products.push({title, description, price, thumbnail, code, stock, id})
+
+        }
+                
+        }                          
         
 
-    }                          
-        
-
-        getProducts = () => {
+        getProducts () {
             return console.log(this.products);
         }
 
-        getProductsById = () => {
+        getProductsById (id) {
 
-            return console.log(this.products);
+            let flag = false;
+
+            this.products.forEach((product)=>{
+                                             
+                if(product.id === id){
+                    console.log(product);
+                    flag = true;
+                    return;
+                    }
+                
+            }
+            )
+
+            if(!flag){
+                        console.log("Product not found!");
+            }
+            
         }
     
 
         }
 
+    console.log("--------------TESTS-------------------");
+
     const newProduct = new ProductManager;
-
-    newProduct.addProduct("mug", "blue mug", 3.50, "/blue-mug.jpeg", 007, 9);
-
     newProduct.getProducts();
+    newProduct.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25);
+    newProduct.getProducts();
+    newProduct.addProduct("producto prueba", "Este es un producto prueba", 200, "sin imagen", "abc123", 25);
+    newProduct.getProducts();
+    newProduct.getProductsById(0);
+    newProduct.getProductsById(9);
+  
+    
 
 
 
-           
+
         
 
 
