a77aa14605236e198f8f614166aa1a45303d2698
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-05-10 17:53:25 -0600

    array to upper

---
+++ class5.js
@@ -0,0 +1,11 @@
+let weekArray = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+
+let upper = [];
+
+ weekArray.forEach((day)=>{
+
+    upper.push(day.toUpperCase());
+ });
+
+ console.table(upper);
+
