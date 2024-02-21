c36e06495c66025ef04162196ba8c2b895f3edae
Mardonio Abreu <mardonio.abreu@protonmail.com>
2024-02-21 11:32:14 -0600

    First approach for Cart and Items class refactoring

--- primera_entrega/src/modules/cart.js
+++ primera_entrega/src/modules/cart.js
@@ -1,9 +1,30 @@
 //Class that creates the shopping cart
 class Cart {
-    constructor(cid){
-                
-        this.cid = cid;
-        this.products = [];
+    
+    static cartArray = [];
+    static cartId = 0;
+
+    static addToCartArray (newCart) {
+        Cart.cartArray.push(newCart);
+    }
+
+    static getCartArray () {
+        return Cart.cartArray;
+    }
+
+    static getCartById (id) {
+        const cart  = Cart.cartArray.find(item => item.id === id);
+        return cart;
+    }
+
+    
+   
+
+
+    constructor (){
+        
+        this.id = Cart.cartId++;
+        this.newCart = [];
     }
 
 }
